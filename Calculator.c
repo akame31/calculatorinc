a875bdcda9eb1d524e5d51adabd49cab7baa362f
AKame <37278780+akame31@users.noreply.github.com>
2018-03-12 00:55:58 +0000

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+        int a;
+
+                printf("Hey, pick a number, and follow the steps: ");
+                scanf("%d", &a);
+
+        int b;
+                printf("Pick another one: \n");
+                scanf("%d", &b);
+
+        int c;
+                printf("Select the signal that you whana use:   ( - or +) \n");
+
+        int d;
+
+                if (d == -) {
+                printf("The results are %d \n", a + b);
+        }
+
+                else {
+                printf("The results are %d", a + b);
+        }
+}
+
+
